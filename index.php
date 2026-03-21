725f0e1f7b7920a9a480275bddca109667d95b00
Makandal <makandalhack@gmail.com>
2026-03-21 19:11:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BatiSCI</title>
+    <link rel="stylesheet" href="/styles/headers.css">
+    <link rel="stylesheet" href="/styles/body.css">
+    <link rel="stylesheet" href="/styles/footers.css">
+</head>
+<body>
+    <header>
+        <div class="logo">BATI SCI</div>
+        <nav>
+            <ul>
+                <li>Acceuil</li>
+                <li>Services</li>
+                <li>R&eacute;alisations</li>
+                <li>A propos</li>
+                <li>Contact</li>
+                <li><button class="cta">Demander un devis</button></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="img">
+        <div id="textacc">
+            <p>"L'INOVATION ET L'INTEGRATION</p>
+            <p>au service </p>
+            <p>de la construction dans le Grand sud"</p>
+        </div>
+        <section id="imglogo">
+            <img src="/img/file_00000000118c71fd9158b4e41e1ed9fd.png" alt="" srcset="">
+        </section>
+    </section>
+    <section class="service">
+        <h4>&Agrave; VOTRE SERVICE</h4>
+        <p>Nous sommes une entreprise sp&eacute;cialiser dans la construction et la renovation,on offre des solutions<br>
+           fiables et durable pour vos projets r&eacute;sidentiels,commerciales et industries.
+        </p>
+        <ul>
+            <li>S&eacute;curit&eacute;</li>
+            <li>Qualit&eacute;</li>
+            <li>Transparence</li>
+        </ul>
+    </section>
+    <section class="services">
+        <h4>NOS SERVICES</h4>
+        <ul>
+            <li>Construction de Batiments</li>
+            <li>R&eacute;novation & Extension</li>
+            <li>Electricit&eacute; & Plomberie</li>
+            <li>Gestion de Projets</li>
+        </ul>
+    </section>
+    <section class="reseau">
+        <h4>NOS R&Eacute;ALISATIONS</h4>
+        <p>Quelques-unes de nos realisations</p>
+        <div class="post">
+
+        </div>
+    </section>
+    <section class="propos">
+        <h4>&Agrave; PROPOS DE NOUS</h4>
+        <p>La soci&eacute;t&eacute; <b>"BATI-sci"</b>est unre entreprise sp&eacute;cialis&eacute;e dans la fabrication d'&eacute;quiperments et dans la<br>
+            construction de batiments r&eacute;sidentiels et commerciaux. Elle est fond&eacute;e par des proffesionnels du domaine de la  <br>
+            construction ayant pour objectif de d&eacute;velopper des solutions modernes et efficaces pour r&eacute;pondre aux besoins du march&eacute;.
+        </p>
+    </section>
+    <footer>
+        <p> Simon Bourdet-Rue M&eacute;dard, Cayes.</p>
+        <p> <a href="">Facebook</a>|<a href="">Linkedin</a>|<a href="">WhatsApp</a> </p>
+    </footer>    
+</body>
+</html>
